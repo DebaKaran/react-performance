9a78a9888c03bc062eb2da5b4d6ac088ba00d3aa
Deba Karan Agrawala <karan.deba@gmail.com>
2026-02-22 23:48:00 +0530

    feat: add button to scroll to a random user in virtualized list
    - Introduced Virtuoso ref for programmatic control
    - Added button to trigger scrollToIndex
    - Enable navigation to a random list item

--- list-virtulaztion/src/virtuoso/UsersApp.jsx
+++ list-virtulaztion/src/virtuoso/UsersApp.jsx
@@ -1,16 +1,23 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { createUsers } from './createUsers'
 import { Virtuoso } from 'react-virtuoso'
 import UserCard from './UserCard';
 
 const UsersApp = () => {
     const [users, setUsers] = useState(createUsers);
+    const virtuosoRef = useRef(null);
 
     return (
         <div>
-            <Virtuoso style={{ height: '600px' }} data={users} itemContent={(_, user) =>
-                <UserCard user={user} />
-            }
+            <button style={{ margin: 40, background: "yellow" }}
+                onClick={() => virtuosoRef.current?.scrollToIndex({
+                    index: Math.random() * users.length,
+                    align: "start"
+                })}>Scroll</button>
+            <Virtuoso ref={virtuosoRef}
+                style={{ height: '600px' }} data={users} itemContent={(_, user) =>
+                    <UserCard user={user} />
+                }
             />
         </div>
     )
